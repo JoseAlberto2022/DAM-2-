20ca0223b62ca24d347f8f0cf74d1d194d808e97
JoseAlberto2022 <118962309+JoseAlberto2022@users.noreply.github.com>
2024-03-12 23:42:46 +0100

    Add files via upload

---
+++ machinelearning/imagenes/002-abrir imagenes.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import os
+
+carpeta = "001-crudo"
+
+archivos = os.listdir(carpeta)
+
+
+for archivo in archivos:
+    imagen = os.path.join(carpeta,archivo)
+    print(imagen)
